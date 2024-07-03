024fbb6c0cf13130bda8c46756df9e774d7ff50d
Aleh Biahun <stones2875@gmail.com>
2024-07-03 10:49:39 +0200

    Solution task number 27

--- HW27/src/Main.java
+++ HW27/src/Main.java
@@ -1,7 +1,33 @@
+import exceptions.WrongLoginException;
+import exceptions.WrongPasswordException;
+
 public class Main {
+
+    private static final String VALID_PATTERN_REGEX = "^[a-zA-Z0-9_]+$";
+
     public static void main(String[] args) {
-        //todo Тут протестить работу метода
+        try {
+            System.out.println(confirmPassword("alehфф", "qwZX12_", "qwZX12_" ));
+        } catch (WrongLoginException e) {
+            System.out.println(e.getMessage());
+        } catch (WrongPasswordException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 
-    //todo тут создать метод confirmPassword()
+    public static boolean confirmPassword(String login, String password, String confirmPassword)
+            throws WrongLoginException, WrongPasswordException {
+        if (validateLoginPassword(login)) {
+            throw new WrongLoginException("\"Login is incorrect\"");
+        }
+        if ((validateLoginPassword(password)) || (!password.equals(confirmPassword))) {
+            throw new WrongPasswordException("\"Password is incorrect\"");
+        }
+        return true;
+    }
+
+    public static boolean validateLoginPassword(String loginPassword) {
+        return !loginPassword.matches(VALID_PATTERN_REGEX) || loginPassword.length() >= 20;
+    }
 }
